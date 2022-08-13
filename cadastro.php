10755bfff977628e701ea76180c8dc799ee2d9e4
Gabiss15 <raquelgabi.nasci1815@gmail.com>
2022-08-13 09:30:20 -0300

    projeto-integrador

---
+++ cadastro.php
@@ -0,0 +1,40 @@
+<?php
+    $numero = $_POST['numero'];
+    $telefone = $_POST['telefone'];
+    $ano = $_POST['ano'];
+    $respFamiliar = $_POST['respFamiliar'];
+    $municipio = $_POST['municipio'];
+    $unidade = $_POST['unidade'];
+    $data = $_POST['data'];
+    $endereco = $_POST['endereco'];
+
+    echo 'Numero de prontuario'.$numero.'</br';
+    echo 'Telefone:'.$telefone.'</br';
+    echo 'Ano:'.$ano.'</br';
+    echo 'Resposavel Familiar'.$respFamiliar.'</br';
+    echo 'Municipio'.$municipio.'</br';
+    echo 'Unidade'.$unidade.'</br';
+    echo 'Data'.$data.'</br';
+    echo 'Endereco'.$endereco.'</br';
+
+    $host = "localhost";
+    $database = "projeto_integrador";
+    #Tabela do cartAo vacinal
+    $username = "root";
+    $password = "";
+
+    #para estabelecer conexao com banco de dados
+    $conexao = mysqli_connect($host, $username, $password, $database);
+    if(!$conexao){
+        die("Conexão Falhou");
+    }
+    #Criar a consulta sql de insercao de dados
+    $sql = "INSERT INTO projeto_integrador VALUES ('$numero', $telefone, $ano, '$respFamiliar', $municipio, $unidade, $data, $endereco, )";
+    if(mysqli_query($conexao,$sql)){
+        echo "Inserido !";
+    }
+    #Encerrar a conexao com o BD
+    mysqli_close($conexao);
+
+    header('Location:index.html');
+?>
